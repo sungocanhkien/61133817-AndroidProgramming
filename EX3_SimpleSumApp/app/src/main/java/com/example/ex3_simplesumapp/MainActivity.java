b4b392820922c7e03304827f268bb30883844fce
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-19 12:53:43 +0700

    Xử lý backend cho EX3_SimpleSumApp

--- EX3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ EX3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void XyLyCong(View view){
+        // Tìm, tham chiếu đến điều khiển trong tệp XML
+        EditText editTextSoA = findViewById(R.id.editA);
+        EditText editTextSoB = findViewById(R.id.editB);
+        EditText editTextSoKQ = findViewById(R.id.editKQ);
+
+        // Lấy dữ liệu về từ điều khiển số a
+        String strA = editTextSoA.getText().toString();
+        // Lấy dữ liệu về từ điều khiển số b
+        String strB = editTextSoB.getText().toString();
+        // Lấy dữ liệu về từ điều khiển số a
+        String strKQ = editTextSoKQ.getText().toString();
+
     }
 }
