e091cdfd9eef3a66b25cae5078ab9a6f87084aaa
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-26 11:50:47 +0700

    Cau2_BMI

---
+++ Cau2_BMI/app/src/test/java/ntu_61133817/cau2_bmi/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ntu_61133817.cau2_bmi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
