9e277332968346468bc8ef61c0064eb96322fb3e
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-26 12:56:34 +0700

    Câu 3 figma

---
+++ Cau3_figma/app/src/main/java/ntu_61133817/figma/android_large___1_activity.java
@@ -0,0 +1,108 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Monday 25th of March 2024 03:08:05 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_large___1_activity extends Activity {
+
+	
+	private View _bg__android_large___1_ek2;
+	private ImageView rectangle_1;
+	private ImageView ellipse_1;
+	private View rectangle_2;
+	private View rectangle_4;
+	private View rectangle_3;
+	private TextView anh_ki_n;
+	private ImageView rectangle_5;
+	private TextView _nh_c_a_t_i;
+	private ImageView rectangle_6;
+	private TextView kho_kho_nh_kh_c;
+	private ImageView rectangle_7;
+	private TextView c_p_nh_t_gi_i_thi_u_b_n_th_n;
+	private ImageView line_1;
+	private ImageView rectangle_8;
+	private TextView b_n__ang_ngh__g__;
+	private TextView _5;
+	private TextView _0;
+	private View rectangle_9;
+	private ImageView rectangle_10;
+	private ImageView rectangle_12;
+	private ImageView rectangle_11;
+	private ImageView rectangle_13;
+	private ImageView rectangle_15;
+	private ImageView rectangle_16;
+	private ImageView rectangle_17;
+	private TextView _17;
+	private TextView _4;
+	private ImageView rectangle_18;
+	private View rectangle_19;
+	private TextView _28_th_ng_3__2021;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___1);
+
+		
+		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
+		rectangle_1 = (ImageView) findViewById(R.id.rectangle_1);
+		ellipse_1 = (ImageView) findViewById(R.id.ellipse_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		anh_ki_n = (TextView) findViewById(R.id.anh_ki_n);
+		rectangle_5 = (ImageView) findViewById(R.id.rectangle_5);
+		_nh_c_a_t_i = (TextView) findViewById(R.id._nh_c_a_t_i);
+		rectangle_6 = (ImageView) findViewById(R.id.rectangle_6);
+		kho_kho_nh_kh_c = (TextView) findViewById(R.id.kho_kho_nh_kh_c);
+		rectangle_7 = (ImageView) findViewById(R.id.rectangle_7);
+		c_p_nh_t_gi_i_thi_u_b_n_th_n = (TextView) findViewById(R.id.c_p_nh_t_gi_i_thi_u_b_n_th_n);
+		line_1 = (ImageView) findViewById(R.id.line_1);
+		rectangle_8 = (ImageView) findViewById(R.id.rectangle_8);
+		b_n__ang_ngh__g__ = (TextView) findViewById(R.id.b_n__ang_ngh__g__);
+		_5 = (TextView) findViewById(R.id._5);
+		_0 = (TextView) findViewById(R.id._0);
+		rectangle_9 = (View) findViewById(R.id.rectangle_9);
+		rectangle_10 = (ImageView) findViewById(R.id.rectangle_10);
+		rectangle_12 = (ImageView) findViewById(R.id.rectangle_12);
+		rectangle_11 = (ImageView) findViewById(R.id.rectangle_11);
+		rectangle_13 = (ImageView) findViewById(R.id.rectangle_13);
+		rectangle_15 = (ImageView) findViewById(R.id.rectangle_15);
+		rectangle_16 = (ImageView) findViewById(R.id.rectangle_16);
+		rectangle_17 = (ImageView) findViewById(R.id.rectangle_17);
+		_17 = (TextView) findViewById(R.id._17);
+		_4 = (TextView) findViewById(R.id._4);
+		rectangle_18 = (ImageView) findViewById(R.id.rectangle_18);
+		rectangle_19 = (View) findViewById(R.id.rectangle_19);
+		_28_th_ng_3__2021 = (TextView) findViewById(R.id._28_th_ng_3__2021);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
