cabaeacd125a8013335c0e64d860d0b41d31942e
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-26 10:30:19 +0700

    updates Cau1

--- Cau1_MayTinhBoTui/src/ntu_61133817/ManHinhTinhToan.java
+++ Cau1_MayTinhBoTui/src/ntu_61133817/ManHinhTinhToan.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class ManHinhTinhToan extends JFrame {
 
@@ -32,11 +37,85 @@ public class ManHinhTinhToan extends JFrame {
 	 */
 	public ManHinhTinhToan() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500, 100, 371, 494);
+		setBounds(500, 100, 379, 494);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(10, 10, 337, 88);
+		contentPane.add(editorPane);
+		
+		JButton btnNewButton = new JButton("9");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton.setBounds(10, 125, 77, 47);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("6");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_1.setBounds(10, 182, 77, 46);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("3");
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_2.setBounds(10, 238, 77, 47);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_4 = new JButton("8");
+		btnNewButton_4.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_4.setBounds(96, 125, 77, 47);
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_1_1 = new JButton("5");
+		btnNewButton_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_1_1.setBounds(96, 182, 77, 46);
+		contentPane.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_2_1 = new JButton("2");
+		btnNewButton_2_1.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_2_1.setBounds(96, 238, 77, 47);
+		contentPane.add(btnNewButton_2_1);
+		
+		JButton btnNewButton_3_1 = new JButton("0");
+		btnNewButton_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_3_1.setBounds(10, 295, 163, 47);
+		contentPane.add(btnNewButton_3_1);
+		
+		JButton btnNewButton_5 = new JButton("7");
+		btnNewButton_5.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_5.setBounds(183, 125, 77, 47);
+		contentPane.add(btnNewButton_5);
+		
+		JButton btnNewButton_1_2 = new JButton("4");
+		btnNewButton_1_2.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_1_2.setBounds(183, 182, 77, 46);
+		contentPane.add(btnNewButton_1_2);
+		
+		JButton btnNewButton_2_2 = new JButton("1");
+		btnNewButton_2_2.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+		btnNewButton_2_2.setBounds(183, 238, 77, 47);
+		contentPane.add(btnNewButton_2_2);
+		
+		JButton btnNewButton_3_2 = new JButton("New button");
+		btnNewButton_3_2.setBounds(183, 295, 77, 47);
+		contentPane.add(btnNewButton_3_2);
+		
+		JButton btnNewButton_6 = new JButton("New button");
+		btnNewButton_6.setBounds(270, 125, 77, 47);
+		contentPane.add(btnNewButton_6);
+		
+		JButton btnNewButton_1_3 = new JButton("New button");
+		btnNewButton_1_3.setBounds(270, 182, 77, 46);
+		contentPane.add(btnNewButton_1_3);
+		
+		JButton btnNewButton_2_3 = new JButton("New button");
+		btnNewButton_2_3.setBounds(270, 238, 77, 47);
+		contentPane.add(btnNewButton_2_3);
+		
+		JButton btnNewButton_3_3 = new JButton("New button");
+		btnNewButton_3_3.setBounds(270, 295, 77, 47);
+		contentPane.add(btnNewButton_3_3);
 	}
-
 }
