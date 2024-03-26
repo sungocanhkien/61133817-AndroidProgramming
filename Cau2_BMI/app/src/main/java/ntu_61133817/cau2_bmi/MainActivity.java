4590264b33b6f6b34cf20576769fa90cd99289d0
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-26 12:09:44 +0700

    Update MainActivity.java

--- Cau2_BMI/app/src/main/java/ntu_61133817/cau2_bmi/MainActivity.java
+++ Cau2_BMI/app/src/main/java/ntu_61133817/cau2_bmi/MainActivity.java
@@ -22,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        chieuCao = findViewById(R.id.chieuCao);
+        canNang = findViewById(R.id.canNang);
+        nam = findViewById(R.id.nam);
+        nu = findViewById(R.id.nu);
+        Tinhbmi=findViewById(R.id.bmi);
+        ketqua = findViewById(R.)
     }
 }
